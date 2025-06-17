2495e4cf203a62b9acc91a4dbe1f6a0552bf46e5
Hung Truong <hungtruongquoc@gmail.com>
2025-06-17 15:35:16 -0500

    Added prose class to activate tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="prose max-w-none">
       <h1 className="text-3xl font-bold text-gray-900 mb-4">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -17,7 +17,7 @@ const App: React.FC = () => {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 };
 
